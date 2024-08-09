e293973cb68e1fbe9026b5294b3cbd71181c99cc
Plamen Bardarov <plamen.bardarov@sap.com>
2024-08-09 11:13:50 +0300

    added failed halth check metrics

---
+++ depot/steps/emit_check_failure_metric_step.go
@@ -0,0 +1,66 @@
+package steps
+
+import (
+	"fmt"
+	"os"
+
+	loggingclient "code.cloudfoundry.org/diego-logging-client"
+	"code.cloudfoundry.org/executor"
+	"github.com/tedsuo/ifrit"
+)
+
+type emitCheckFailureMetricStep struct {
+	subStep       ifrit.Runner
+	path          string
+	port          int
+	timeout       int
+	checkProtocol executor.CheckProtocol
+	checkType     executor.HealthcheckType
+	metronClient  loggingclient.IngressClient
+}
+
+const (
+	CheckFailedCount = "ChecksFailedCount"
+)
+
+func NewEmitCheckFailureMetricStep(
+	subStep ifrit.Runner,
+	path string,
+	port int,
+	timeout int,
+	checkProtocol executor.CheckProtocol,
+	checkType executor.HealthcheckType,
+	metronClient loggingclient.IngressClient) ifrit.Runner {
+	return &emitCheckFailureMetricStep{
+		subStep:       subStep,
+		path:          path,
+		port:          port,
+		timeout:       timeout,
+		checkProtocol: checkProtocol,
+		checkType:     checkType,
+		metronClient:  metronClient,
+	}
+}
+
+func (step *emitCheckFailureMetricStep) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
+	if step.subStep == nil {
+		return nil
+	}
+
+	subStepErr := step.subStep.Run(signals, ready)
+
+	if subStepErr != nil {
+		step.emitFailureMetric()
+	}
+
+	return subStepErr
+}
+
+func (step *emitCheckFailureMetricStep) emitFailureMetric() {
+	metricName := constructMetricName(step.checkProtocol, step.checkType)
+	go step.metronClient.IncrementCounter(metricName)
+}
+
+func constructMetricName(checkProtocol executor.CheckProtocol, checkType executor.HealthcheckType) string {
+	return fmt.Sprintf("%s%s%s", checkProtocol, checkType, CheckFailedCount)
+}
